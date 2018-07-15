9c196d13c35effa0ee4c99d38b154d1571949415
Bullwinkle <developer085@gmail.com>
2018-07-15 22:10:56 +0300

    function types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
 console.log('hello typescript!');
+
+type TFactor = 1 | 2;
+type TSetCost = (newCost:number, factor: TFactor) => number;
+
+const setCost: TSetCost = function (x, y) {
+	return x * y;
+};
+
+setCost(1,2);
