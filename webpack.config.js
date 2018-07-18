f8c0a1067d496c3222917d995b13d9d9511c2ff8
Bullwinkle <developer085@gmail.com>
2018-07-18 15:56:14 +0300

    basic TS starter

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
 	mode: "development",
 	devtool: "source-map",
-	entry: "./src/app.ts",
+	entry: "./src/main.ts",
 	output: {
 		path: path.resolve(__dirname,'dist'),
-		filename: "app.js"
+		filename: "main.js"
 	},
 	resolve: {
 		extensions: ['.ts','.js']
